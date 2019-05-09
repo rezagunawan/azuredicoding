40896aea47ad2e2d8025f8c4b739310d95af3e25
rezagunawan <rezagunawan999@gmail.com>
2019-05-09 15:38:39 +0700

    file blob

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 	if( $conn ) {
     
 	}else{
-	     echo "Connection could not be established.<br />";
-	     die( print_r( sqlsrv_errors(), true));
+	     // echo "Connection could not be established.<br />";
+	     // die( print_r( sqlsrv_errors(), true));
 	}
 
 	if(isset($_POST['submit'])){
